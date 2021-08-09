950a15b061032f10c5acfd02ed8dd49ef316845a
SIVAJI KADAPALA <83819803+sivaji-Kadapala@users.noreply.github.com>
2021-08-09 12:37:44 +0530

    Update ConditionalRendering.js

--- src/components/ConditionalRendering.js
+++ src/components/ConditionalRendering.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react'
 
-export default class ConditionalRendering extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             isLoggedIn:true
-        }
+export default class ConditionalRendering extends Component {constructor(props) {
+    super(props)
+
+    this.state = {
+         isLogged:false
     }
-    
-    render() {
-     return (
-<div>
-    {
-        this.state.isLoggedIn?<h1>helllloo</h1>:<h2>hiiiii</h2>
-    }
-</div>)
-              
-       
-        // {
-        //     return  <div>Welcome Siva</div>
-        // }else{
-        //     return  <div>Welcome Sivaji</div>
-        // }
-        
 }
+
+    render() {
+        if(this.state.isLogged){
+            return(
+                <div>Welcome sivaji</div>
+            )
+        }else{
+            return(
+                <div>Welcome jagadheesh</div>
+            )
+        }
+        
+    }
 }
